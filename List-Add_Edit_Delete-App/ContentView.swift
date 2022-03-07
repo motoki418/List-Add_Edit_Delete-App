345966ba5cd4d78080ac38edc7aeb0d7486387be
motoki418 <snkrs418@gmail.com>
2022-03-07 18:17:27 +0900

    Initial Commit

---
+++ List-Add_Edit_Delete-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  List-Add_Edit_Delete-App
+//
+//  Created by nakamura motoki on 2022/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ List-Add_Edit_Delete-App/List_Add_Edit_Delete_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_Add_Edit_Delete_AppApp.swift
+//  List-Add_Edit_Delete-App
+//
+//  Created by nakamura motoki on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct List_Add_Edit_Delete_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
